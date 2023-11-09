54472de0ef089ae6be987ed8f38a2ccab0f6b321
amilasampath421 <alahakoonamila0@gmail.com>
2023-11-10 03:27:14 +0530

    adding input field
    adding input field date

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -6,5 +6,15 @@ export default function MainPage() {
         <h1 className=" font-bold text-5xl lg:mx-32 text-green-500">Convert Your Currency Today</h1>
         <p className="lg:mx-32 opacity-40 py-6">you can convert any Currency in your destination Currency with live update</p>
     </div>
+    <div>
+      <section>
+        <form>
+          <div>
+            <label>Date</label>
+            <input type="date"></input>
+          </div>
+        </form>
+      </section>
+    </div>
   )
 }
