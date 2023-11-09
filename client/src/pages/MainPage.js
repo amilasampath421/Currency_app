28770f6feecea3be5fbab7a394ce89c900f98624
amilasampath421 <alahakoonamila0@gmail.com>
2023-11-10 03:08:17 +0530

    main title style changed
    main title style changed  title-bold ,padding included

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MainPage() {
+  return (
+    <div>
+        <h1 className=" font-bold text-5xl lg:mx-32 text-green-500">Convert Your Currency Today</h1>
+        <p className="lg:mx-32 opacity-40 py-6">you can convert any Currency in your destination Currency with live update</p>
+    </div>
+  )
+}
